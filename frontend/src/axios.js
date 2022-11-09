24a183d429f606bcc034ef3cc517dc99fe2a1645
Pinsyuan <b07901162@g.ntu.edu.tw>
2022-11-10 00:37:29 +0800

    Update url in axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const instance = axios.create({baseURL: 'http://localhost:4000/api/course'})
+const instance = axios.create({baseURL: `http://${process.env.PORT}/api/course`})
 
 const course = async() => {
     const {data: {coursesList}} = await instance.get('/course');

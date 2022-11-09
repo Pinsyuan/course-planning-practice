95266b03c90c024596a6dbc514fbb352df283992
Pinsyuan <b07901162@g.ntu.edu.tw>
2022-11-10 01:43:41 +0800

    Update url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
-const instance = axios.create({baseURL: `https://course-planning-practice.herokuapp.com/api/course`})
+// const instance = axios.create({baseURL: `https://course-planning-practice.herokuapp.com/api/course`})
 // const instance = axios.create({baseURL: `${process.env.HOST}:${process.env.PORT}/api/course`})
+const instance = axios.create({baseURL: `http://localhost:4000//api/course`})
+console.log(`url in axios: ${instance}`)
 
 const course = async() => {
-    console.log("get from axios")
     const {data: {coursesList}} = await instance.get('/course');
     return coursesList
 }

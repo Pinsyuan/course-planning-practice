8569bb96586704c4a7e8061b76242a2526224132
Pinsyuan <b07901162@g.ntu.edu.tw>
2022-11-10 11:14:59 +0800

    Modify url in axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // const instance = axios.create({baseURL: `https://course-planning-practice.herokuapp.com/api/course`})
-// const instance = axios.create({baseURL: `${process.env.HOST}:${process.env.PORT}/api/course`})
-const instance = axios.create({baseURL: `http://localhost:4000//api/course`})
+const instance = axios.create({baseURL: `${process.env.HOST}:${process.env.PORT}/api/course`})
+// const instance = axios.create({baseURL: `http://localhost:4000//api/course`})
 console.log(`url in axios: ${instance}`)
 
 const course = async() => {
